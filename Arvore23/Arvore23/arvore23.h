6c0dfcf217e5d6ecbc16ef89261b89c290ee6b9e
Marcos Sousa <sousas1569@gmail.com>
2025-08-10 21:20:57 -0300

    remoção incorreta removida

--- Arvore23/Arvore23/arvore23.h
+++ Arvore23/Arvore23/arvore23.h
@@ -49,41 +49,6 @@ Arvore23 cria_no(int chave_esq, int chave_dir, Arvore23 esq, Arvore23 meio, Arvo
 // Pos-condicao: insere elemento
 Arvore23 inserir(Arvore23 raiz, int chave);
 
-// Funcao para encontrar sucessor
-// Pre-condicao: nenhuma
-// Pos-condicao: encontra menor valor da subarvore da direita
-Arvore23 encontrar_sucessor(Arvore23 raiz, int *sucessor);
-
-// Funcao para corrigir underflow
-// Pre-condicao: nenhuma
-// Pos-condicao: corrige underflow distribuindo a esquerda, direita ou fundindo filhos
-void restaurar_subarvore(Arvore23 pai, int pos_filho);
-
-// Funcao para distribuir a esquerda
-// Pre-condicao: nenhuma
-// Pos-condicao: reajusta chaves sem alterar a altura da arvore
-void redistribuir_esquerda(Arvore23 pai, int pos_filho);
-
-// Funcao para distribuir a direita
-// Pre-condicao: nenhuma
-// Pos-condicao: reajusta chaves sem alterar a altura da arvore
-void redistribuir_direita(Arvore23 pai, int pos_filho);
-
-// Funcao para fundir irmaos
-// Pre-condicao: nenhuma
-// Pos-condicao: funde um filho da esquerda com o irmao da direita
-void merge(Arvore23 pai, int pos_filho);
-
-// Funcao recursiva para remover elemento
-// Pre-condicao: nenhuma
-// Pos-condicao: remove elemento
-Arvore23 remover_recursivo(Arvore23 raiz, int chave, int *fim);
-
-// Funcao para remover elemento
-// Pre-condicao: arvore criada
-// Pos-condicao: remove elemento
-Arvore23 remover(Arvore23 raiz, int chave);
-
 // Funcao para imprimir arvore23
 // Pre-condicao: nenhuma
 // Pos-condicao: imprime arvore em ordem na tela
--- Arvore23/main.c
+++ Arvore23/main.c
@@ -4,15 +4,26 @@ int main()
 {
     Arvore23 raiz = criar_arvore();
 
-    raiz = inserir(raiz, 5);
-    raiz = inserir(raiz, 10);
+    raiz = inserir(raiz, 4);
+    raiz = inserir(raiz, 6);
     raiz = inserir(raiz, 15);
-    raiz = inserir(raiz, 20);
-    raiz = inserir(raiz, 25);
-    raiz = inserir(raiz, 30);
-
-    raiz = remover(raiz, 5);
-    raiz = remover(raiz, 25);
+    raiz = inserir(raiz, 11);
+    raiz = inserir(raiz, 9);
+    raiz = inserir(raiz, 50);
+    raiz = inserir(raiz, 21);
+    raiz = inserir(raiz, 34);
+    raiz = inserir(raiz, 23);
+    raiz = inserir(raiz, 13);
+    raiz = inserir(raiz, 7);
+    raiz = inserir(raiz, 14);
+    raiz = inserir(raiz, 18);
+    raiz = inserir(raiz, 60);
+    raiz = inserir(raiz, 90);
+    raiz = inserir(raiz, 17);
+    raiz = inserir(raiz, 8);
+    raiz = inserir(raiz, 91);
+    raiz = inserir(raiz, 92);
+    raiz = inserir(raiz, 12);
 
     imprimir_niveis(raiz);
 
